4835f321eff82ff405ae920f6555da8d254477fc
microdimmer <dimmer1988@gmail.com>
2018-11-29 17:47:39 +0500

    0.0.8
    Remove iarduino library

--- GSMrelay.ino
+++ GSMrelay.ino
@@ -5,7 +5,8 @@ const char PROG_VERSION[] = "0.1";
 #include <DFMiniMp3.h>    //DF MP3 Player mini
 #include <ClickEncoder.h> //encoder with button https://github.com/0xPIT/encoder
 #include <TimeLib.h>      //timekeeping
-#include <iarduino_GSM.h> //for GSM modem A6
+// #include <iarduino_GSM.h> //for GSM modem A6
+#include <SoftwareSerial.h> //for GSM modem A6
 
 OneWire ds(12);
 byte DSaddr1[8] = {0x28, 0xFF, 0xE4, 0x47, 0x50, 0x17, 0x04, 0x73}; // first DS18B20 adress
@@ -15,7 +16,7 @@ ClickEncoder encoder(A1, A0, A2, 2); //changed to 2 at one turn
 ClickEncoder::Button button;
 int16_t last_val, enc_val;
 
-iarduino_GSM gsm;
+// iarduino_GSM gsm;
 SoftwareSerial gsmSerial(2, 3);
 char callnumber[13];
 
@@ -27,7 +28,7 @@ SimpleTimer timer;
 LCD_1602_RUS lcd(0x3F, 16, 2);
 
 int8_t REL_PIN = 11; //pin for relay1
-int8_t GSM_PIN = 9; //pin for GSM
+int8_t GSM_PIN = 9;  //pin for GSM
 
 #ifdef DEBUG
 #define PRINTLNF(s)       \
@@ -63,6 +64,8 @@ bool relayFlag = false;
 
 bool updateMainScreen = true;
 
+bool GSMTimeoutFlag = false;
+
 uint8_t current_menu = 0; //0 - homepage, 1 - main menu, 2 - info menu
 LiquidLine main_line1(1, 0, "Power");
 LiquidLine main_line2(1, 1, "Shedule");
@@ -96,6 +99,7 @@ void go_info_menu()
   menu_system.change_menu(info_menu);
   menu_system.change_screen(1);
   menu_system.switch_focus();
+  current_menu = 2;
 }
 
 void go_main_menu()
@@ -103,6 +107,7 @@ void go_main_menu()
   PRINTLNF("changing to main menu!");
   menu_system.switch_focus();
   menu_system.change_menu(main_menu);
+  current_menu = 1;
 }
 
 void go_main_screen()
@@ -117,7 +122,7 @@ void go_main_screen()
 
 void readEncoder()
 {
-  if (current_menu = 0)
+  if (current_menu == 0)
     return;
   encoder.service();
   enc_val += encoder.getValue(); //read encoder
@@ -163,7 +168,6 @@ void readButton()
       menu_system.switch_focus();
       updateMainScreen = false;
       current_menu = 1; //change to main menu
-      PRINTLNF("show menu");
     }
     else
     { // go to attached to menu function
@@ -208,7 +212,7 @@ void drawMainSreen()
     lcd.print("+");
   lcd.print(t_home, 1);
   lcd.print("°C");
-  PRINTLNF("show main screen");
+  // PRINTLNF("show main screen");
   current_menu = 0;
   updateMainScreen = false;
 }
@@ -222,55 +226,77 @@ void updateScr()
   lcd.print("°C");
 }
 
-void readMeasurements()
+void readMeasurements() //send request to temp sensor
+{
+  ds.reset();
+  ds.select(DSaddr1);
+  ds.write(0x44, 0); // start conversion
+
+  ds.reset();
+  ds.select(DSaddr1);
+  ds.write(0xBE, 0); // read Scratchpad
+
+  timer.setTimeout(200L, readDSresponse);
+}
+
+void readDSresponse() //read response from sensor
 {
   DSdata1[0] = ds.read();
   DSdata1[1] = ds.read();
   t_home = (float)((int)DSdata1[0] | (((int)DSdata1[1]) << 8)) * 0.0625 + 0.03125;
-  sendDScommand(DSaddr1);
 
-  if (current_menu == 0)
+  if (current_menu == 0) //if main screen is displayed,
     updateMainScreen = true;
-  // updateScr();
 }
 
-void sendDScommand(byte adr[8])
+void setTimenow() //send request to GSM
 {
-  ds.reset();
-  ds.select(adr);
-  ds.write(0x44, 0); // start conversion
+  while (gsmSerial.available() > 0) //clean UART buffer
+    gsmSerial.read();
 
-  ds.reset();
-  ds.select(adr);
-  ds.write(0xBE, 0); // read Scratchpad
+  gsmSerial.println(F("AT+CCLK?"));
+  PRINTLNF("send request command to GSM");
+  timer.setTimeout(500L, readGSMresponse);
 }
 
-void setTimenow()
+void readGSMresponse() //read response from GSM
 {
-  String timestr = gsm.runAT("AT+CCLK?\r\n"); //return command like   +CCLK: "18/11/22,06:52:06+05"
-  byte Year, Month, Day, Hour, Minute, Second, timeZone;
-  Year = timestr.substring(10, 12).toInt();
-  Month = timestr.substring(13, 15).toInt();
-  Day = timestr.substring(16, 18).toInt();
-  Hour = timestr.substring(19, 21).toInt();
-  Minute = timestr.substring(22, 24).toInt();
-  Second = timestr.substring(25, 27).toInt();
-  timeZone = timestr.substring(28, 30).toInt();
+  PRINTLNF("trying to sync clock");
+
+  char i;
+  String strBuffer;
+  if (gsmSerial.find("+CCLK:")) //return command like +CCLK: "18/11/29,07:34:36+05"
+  {
+    while (gsmSerial.available() > 0)
+    {
+      i = (gsmSerial.read());
+      strBuffer += i;
+    }
+  }
+
+  byte Year, Month, Day, Hour, Minute, Second, timeZone; //return command like +CCLK: "18/11/29,07:34:36+05"
+  Year = strBuffer.substring(2, 4).toInt();
+  Month = strBuffer.substring(5, 7).toInt();
+  Day = strBuffer.substring(8, 10).toInt();
+  Hour = strBuffer.substring(11, 13).toInt();
+  Minute = strBuffer.substring(14, 16).toInt();
+  Second = strBuffer.substring(17, 19).toInt();
+  timeZone = strBuffer.substring(20, 22).toInt();
   setTime(Hour, Minute, Second, Day, Month, Year);
   adjustTime(timeZone * SECS_PER_HOUR); //set timezone
 }
 
 void switchRelay()
 {
-  if (gsm.CALLavailable(callnumber))
-  {
-    PRINTLN("calling number ", callnumber);
-    gsm.CALLend();
-    relayFlag = !relayFlag;
-    digitalWrite(REL_PIN, relayFlag);
-    if (current_menu == 0)
-      updateMainScreen = true;
-  }
+  // if (gsm.CALLavailable(callnumber))
+  // {
+  //   PRINTLN("calling number ", callnumber);
+  //   gsm.CALLend();
+  //   relayFlag = !relayFlag;
+  //   digitalWrite(REL_PIN, relayFlag);
+  //   if (current_menu == 0)
+  //     updateMainScreen = true;
+  // }
 }
 
 void initGSM()
@@ -281,23 +307,26 @@ void initGSM()
   digitalWrite(GSM_PIN, LOW);
   delay(2000);
   digitalWrite(GSM_PIN, HIGH);
-  
+
+  PRINTLNF("trying to set 9600");
   for (size_t i = 0; i < 4; i++)
   {
-    gsmSerial.println(F("AT+IPR=9600")); //trying to set 9600 
-    PRINTLNF("trying to set 9600");
+    gsmSerial.println(F("AT+IPR=9600")); //trying to set 9600
     delay(50);
   }
   gsmSerial.begin(9600);
   while (true)
   {
     gsmSerial.println(F("AT+CPAS"));
-    if (gsmSerial.find("+CPAS:0")) break;
+    if (gsmSerial.find("+CPAS:0"))
+      break;
     // while (gsmSerial.available())
-      // Serial.write(gsmSerial.read()); //Forward what Software Serial received to Serial Port
+    // Serial.write(gsmSerial.read()); //Forward what Software Serial received to Serial Port
+    (millis() / 500 % 2) ? lcd.print(F("загрузка..")) : lcd.print(F("загрузка. "));
     delay(500);
     PRINTLNF("initialize GSM...");
   }
+  // gsmSerial.println(F("ATE0"));
   PRINTLNF("init GSM OK");
 }
 
@@ -317,7 +346,7 @@ void setup()
 
   Serial.begin(9600);
 
-  sendDScommand(DSaddr1); //request temp from DS sensor
+  // sendDScommand(DSaddr1); //request temp from DS sensor
 
   initGSM();
   // gsm.begin(gsmSerial); //init GSM module
@@ -332,7 +361,7 @@ void setup()
   // gsm.runAT(F("ATE0\r\n"));
   // PRINTLN("time is", gsm.runAT("AT+CCLK?\r\n"));
 
-  // setTimenow(); //request time from GSM and sync internal clock
+  setTimenow(); //request time from GSM and sync internal clock
 
   menu_system.set_focusPosition(Position::LEFT); //init menu system
   main_line1.attach_function(1, func);
@@ -350,8 +379,9 @@ void setup()
 
   readMeasurements();
 
-  timer.setInterval(1000L, readMeasurements);
+  timer.setInterval(5000, readMeasurements);
   timer.setInterval(SECS_PER_HOUR * 6 * 1000L, setTimenow); //sync time every 6 hours
+  // timer.setInterval(5000L, setTimenow); //sync time every 6 hours
   // timer.setInterval(5000L, updateMainScreen);
   // timer.setInterval(1L, timerIsr);
 
