1a57d49dfd370d4da7ca00f02ba6b5fa1b6afc8d
microdimmer <dimmer1988@gmail.com>
2018-12-04 17:39:59 +0500

    0.0.9
    time sync

--- GSMrelay.ino
+++ GSMrelay.ino
@@ -1,4 +1,5 @@
 const char PROG_VERSION[] = "0.1";
+const char PHOME_NUMBER[] = "79227754426";
 #define DEBUG
 
 #include <OneWire.h>      //for DS18B20
@@ -21,6 +22,7 @@ SoftwareSerial gsmSerial(2, 3);
 char callnumber[13];
 
 String timeString = ""; //TODO
+String telNumber = "";
 
 #include <SimpleTimer.h> // Handy timers
 SimpleTimer timer;
@@ -217,15 +219,6 @@ void drawMainSreen()
   updateMainScreen = false;
 }
 
-void updateScr()
-{
-  lcd.setCursor(9, 1);
-  if (t_home > 0)
-    lcd.print("+");
-  lcd.print(t_home, 1);
-  lcd.print("°C");
-}
-
 void readMeasurements() //send request to temp sensor
 {
   ds.reset();
@@ -249,25 +242,24 @@ void readDSresponse() //read response from sensor
     updateMainScreen = true;
 }
 
-void setTimenow() //send request to GSM
+void requestTime() //send request to GSM
 {
   while (gsmSerial.available() > 0) //clean UART buffer
     gsmSerial.read();
 
   gsmSerial.println(F("AT+CCLK?"));
-  PRINTLNF("send request command to GSM");
-  timer.setTimeout(500L, readGSMresponse);
+  PRINTLNF("request time from GSM");
+  // timer.setTimeout(20L, readGSMresponse);
 }
 
 void readGSMresponse() //read response from GSM
 {
   PRINTLNF("trying to sync clock");
-
   char i;
   String strBuffer;
   if (gsmSerial.find("+CCLK:")) //return command like +CCLK: "18/11/29,07:34:36+05"
   {
-    while (gsmSerial.available() > 0)
+    while (gsmSerial.available())
     {
       i = (gsmSerial.read());
       strBuffer += i;
@@ -284,19 +276,7 @@ void readGSMresponse() //read response from GSM
   timeZone = strBuffer.substring(20, 22).toInt();
   setTime(Hour, Minute, Second, Day, Month, Year);
   adjustTime(timeZone * SECS_PER_HOUR); //set timezone
-}
-
-void switchRelay()
-{
-  // if (gsm.CALLavailable(callnumber))
-  // {
-  //   PRINTLN("calling number ", callnumber);
-  //   gsm.CALLend();
-  //   relayFlag = !relayFlag;
-  //   digitalWrite(REL_PIN, relayFlag);
-  //   if (current_menu == 0)
-  //     updateMainScreen = true;
-  // }
+  PRINTLNF("sync clock OK");
 }
 
 void initGSM()
@@ -315,19 +295,93 @@ void initGSM()
     delay(50);
   }
   gsmSerial.begin(9600);
+  String loading = F("загрузка");
   while (true)
   {
     gsmSerial.println(F("AT+CPAS"));
     if (gsmSerial.find("+CPAS:0"))
       break;
-    // while (gsmSerial.available())
-    // Serial.write(gsmSerial.read()); //Forward what Software Serial received to Serial Port
-    (millis() / 500 % 2) ? lcd.print(F("загрузка..")) : lcd.print(F("загрузка. "));
+
+    lcd.setCursor(3, 1); // display loading message
+    lcd.print(loading);
+    loading.trim();
+    loading += ".   ";
+    if (loading.length() > 24)
+      loading = loading.substring(0, 16);
     delay(500);
     PRINTLNF("initialize GSM...");
   }
-  // gsmSerial.println(F("ATE0"));
+  gsmSerial.println(F("ATE0"));
   PRINTLNF("init GSM OK");
+  while (gsmSerial.available()) //clean UART buffer
+    gsmSerial.read();
+}
+
+void readStringGSM()
+{
+  String strBuffer;
+  // while (gsmSerial.available())
+  //   Serial.write(gsmSerial.read()); //Forward what Software Serial received to Serial Port
+
+  while (gsmSerial.available())
+  {
+
+    //   // PRINTLN("GSM string=",gsmSerial.readStringUntil("\n"));
+    //   // PRINTLN("GSM string=",gsmSerial.readStringUntil("\0"));
+    //   // PRINTLN("GSM string=",gsmSerial.readStringUntil("\r"));
+    strBuffer = gsmSerial.readStringUntil("\0");
+    //   // strBuffer = gsmSerial.readString();
+    //   // strBuffer = gsmSerial.readStringUntil("\r");
+    // PRINTLN("GSM string=", strBuffer);
+    break;
+    //   // PRINTLN("strBuffer.indexOf",strBuffer.indexOf("RING"));
+  }
+
+  if ((strBuffer.indexOf("RING") > 0))
+  {
+    gsmSerial.println(F("AT+CLCC"));
+    PRINTLNF("ringin!");
+  }
+  else if (strBuffer.indexOf(PHOME_NUMBER) > 0)
+  {
+    gsmSerial.println(F("AT+CHUP"));
+    relayFlag = !relayFlag;
+    digitalWrite(REL_PIN, relayFlag);
+    PRINTLNF("relay switch!");
+    // lcd.clear();
+    if (current_menu == 0)
+      updateMainScreen = true;
+
+    while (gsmSerial.available()) //clean UART buffer
+      gsmSerial.read();
+  }
+  else if (strBuffer.indexOf("+CCLK:") > 0) //return command like +CCLK: "18/11/29,07:34:36+05"
+  {
+    strBuffer = strBuffer.substring(strBuffer.indexOf("+CCLK:")+8, 30);//return 18/11/29,07:34:36+05
+    PRINTLN("time string=", strBuffer);
+    byte parse_time_arr[7]; // Year, Month, Day, Hour, Minute, Second, timeZone                                
+    memset(parse_time_arr, 0, sizeof(parse_time_arr));     //set zeros
+    for (byte i = 0, str_index; i < 7; i++,str_index += 3)
+    {
+      parse_time_arr[i] = strBuffer.substring(str_index, str_index + 2).toInt(); // Year, Month, Day, Hour, Minute, Second, timeZone
+      if (parse_time_arr[i] == 0)
+      {
+        PRINTLN("parse_time_arr[i]=", parse_time_arr[i]);
+        PRINTLNF("sync clock fault!");
+        return;
+      }
+    }
+    // PRINTLN("Hour ", parse_time_arr[3]);
+    // PRINTLN("Minute ", parse_time_arr[4]);
+    // PRINTLN("Second ", parse_time_arr[5]);
+    // PRINTLN("Day ", parse_time_arr[0]);
+    // PRINTLN("Month ", parse_time_arr[1]);
+    // PRINTLN("Year ", parse_time_arr[2]);
+    // PRINTLN("timeZone ", parse_time_arr[6]);
+    setTime(parse_time_arr[3], parse_time_arr[4], parse_time_arr[5], parse_time_arr[0], parse_time_arr[1], parse_time_arr[2]); //set time and date
+    adjustTime(parse_time_arr[6] * SECS_PER_HOUR);//set timezone
+    PRINTLNF("sync clock OK");
+  }
 }
 
 void setup()
@@ -337,6 +391,7 @@ void setup()
 
   lcd.init(); //init display
   lcd.backlight();
+  lcd.clear();
   lcd.setCursor(1, 0);
   lcd.print(F("GSM-реле v."));
   lcd.setCursor(12, 0);
@@ -348,21 +403,10 @@ void setup()
 
   // sendDScommand(DSaddr1); //request temp from DS sensor
 
-  initGSM();
-  // gsm.begin(gsmSerial); //init GSM module
-  // while (gsm.status() != GSM_OK)
-  // {
-  //   delay(1000);
-  //   PRINTLN("status ", gsm.status());
-  //   lcd.setCursor(3, 1);
-  //   (millis() / 500 % 2) ? lcd.print(F("загрузка..")) : lcd.print(F("загрузка. "));
-  // }
-
-  // gsm.runAT(F("ATE0\r\n"));
-  // PRINTLN("time is", gsm.runAT("AT+CCLK?\r\n"));
-
-  setTimenow(); //request time from GSM and sync internal clock
-
+  initGSM(); //init GSM module
+  delay(1000);
+  requestTime();                                 //request time from GSM and sync internal clock
+  readMeasurements();                            //request temp
   menu_system.set_focusPosition(Position::LEFT); //init menu system
   main_line1.attach_function(1, func);
   main_line2.attach_function(1, func);
@@ -375,25 +419,22 @@ void setup()
   info_line4.attach_function(1, go_main_menu);
   // menu_system.switch_focus();
 
-  // setTime(12, 59, 40, 01, 01, 19);
-
-  readMeasurements();
-
   timer.setInterval(5000, readMeasurements);
-  timer.setInterval(SECS_PER_HOUR * 6 * 1000L, setTimenow); //sync time every 6 hours
-  // timer.setInterval(5000L, setTimenow); //sync time every 6 hours
+  timer.setInterval(SECS_PER_HOUR * 6 * 1000L, requestTime); //sync time every 6 hours
+  // timer.setInterval(5000L, requestTime); //sync time every 6 hours
   // timer.setInterval(5000L, updateMainScreen);
   // timer.setInterval(1L, timerIsr);
 
   lcd.clear();
   drawMainSreen();
+
+  gsmSerial.setTimeout(100); //set timeout for readString() func
 }
 void loop()
 {
   timer.run();
   readButton();
   readEncoder();
-
   drawMainSreen();
-  // switchRelay();
+  readStringGSM();
 }
